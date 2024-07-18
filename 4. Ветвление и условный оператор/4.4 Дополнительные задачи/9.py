e1c379ccf59f52d56dfa99f7072fd0c4604272fd
RAX3R <45417916+vqpv@users.noreply.github.com>
2024-07-18 23:23:47 +0300

    4.4 - 9

---
+++ 4. Ветвление и условный оператор/4.4 Дополнительные задачи/9.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n in range(90, 100):
+    print("A")
+elif n in range(80, 89):
+    print("B")
+elif n in range(70, 79):
+    print("C")
+elif n in range(60, 69):
+    print("D")
+elif n in range(0, 59):
+    print("F")
